63ad5e5f16914efc5ec0190e7db9fcc23a06fb18
ullahhab <61397521+ullahhab@users.noreply.github.com>
2020-03-24 02:46:52 -0500

    Create scheduleEvent.h

---
+++ scheduleEvent.h
@@ -0,0 +1,18 @@
+#include "emane/event.h"
+#include "emane/events/eventsids.h"
+#include "emane/events/slotinfo.h"
+#include "emane/events/slotstructure.h"
+
+
+
+#include <memory>
+
+namespace EMANE
+{
+  namespace Events
+   {
+    class HBScheduleEvent: public Event
+    {
+    public:
+    using Frequencies = std::set<unit64_t>;
+    HBScheduleEvent(const Serialization 
