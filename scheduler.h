c46f68f06573922be1bc52358b18544103ae1e86
ullahhab <61397521+ullahhab@users.noreply.github.com>
2020-04-05 16:10:18 -0500

    Update scheduler.h

--- scheduler.h
+++ scheduler.h
@@ -6,3 +6,44 @@
 #include "emane/runningstatemutable.h"
 #include "emane/upstreampacket.h"
 
+#include "models/heavyball/type.h"
+//TODO: LINE MIGHT CHANGE!!!
+#include "emane/models/tdma/scheduleruser.h"
+
+
+
+namespace EMANE
+{
+  namespace Models
+  {
+    namespace HeavyBall
+    {
+      class Scheduler : public Component,
+                        public PlatformServiceUser,
+                        public RunningStateMutable
+                        
+       {
+       public:
+       
+       //TODO: MAYBE SCHeduler need to know who needs to go first or would that be multihop part!! Comes after implementation.
+       
+       
+        virtual ~Scheduler(){};
+        virtual void processSchedulerPacket(UpstreamPacket & pkt,
+                                            const PacketMetaInfo & packetMetaInfo) = 0;
+        virtual voidprocessPacketMetaInfo*const PacketMetaInfo & packetMetaInfo) = 0;
+        
+        protected:
+          SchedulerUser * pSchedulerUser_;
+          NEMId id_;
+          
+          Scheduler(NEMId id, PlatformServiceProvider * pPlatformServiceProvider, SchedulerUser * pSchedulerUser);
+          PlatformServiceUser{pPlatformServiceProvider},
+          pSchedulerUser_{pSchedulerUser},
+          id_{id}{}
+          };
+    }
+  }
+}
+
+           
