4cbfe033a3196c44c41d81687e89f7d3b4a4e1c9
ullahhab <61397521+ullahhab@users.noreply.github.com>
2020-04-19 12:04:52 -0500

    Update HeavyballshimLayerImpl.h

--- HeavyballshimLayerImpl.h
+++ HeavyballshimLayerImpl.h
@@ -1,1 +1,23 @@
+#include "emane/nemlayer.h"
+#include "emane/radioserviceuser.h"
+//TODO:ADD ONE MORE FILE TO GET THE BYTES PER SECONDS SPEED.
+#include ""
 
+namespace Emane
+{
+  class HeavyBallShimLayerImplementor : public NEMLayer,
+                                        public RadioServiceUser,
+                                        public channel_state
+                                        
+  public:
+    virtual ~HeavyBallShimLayerImplementor() {}
+  
+  protected:
+    HeavyBallShimLayerImplementor(NEMId id,
+                                  PlatformServiceProvider * pPlatformServiceProvider,
+                                  RadioServiceProvider * pRadioServiceProvider,
+                                  //TODO: SOMETHING LIKE BYTESPERSECOND FOR CHANNELSTATE
+                                  NONE)
+      
+      
+   NEM
