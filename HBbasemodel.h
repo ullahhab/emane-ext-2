b9af7a061a077c08394ab914ca9c1189f582c00c
ullahhab <61397521+ullahhab@users.noreply.github.com>
2020-04-20 13:15:36 -0500

    Create HBbasemodel.h

---
+++ HBbasemodel.h
@@ -0,0 +1,14 @@
+#ifdef EMANEHBBASEMODEL_HEADER_
+#define EMANEHBBASEMODEL_HEADER_
+
+#include "heavyballshimLayerImpl.h"
+#include "emane/models/HeavyBall/scheduler.h"
+#inlcude "emane/models/tdma/queuemanager.h"
+
+namespace EMANE
+{
+  namespace Models
+  {
+    namespace HeavyBall
+    {
+      class BaseModel : public 
