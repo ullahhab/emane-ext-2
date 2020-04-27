1f54ceeb5d9ad43651d587209a280ebdf71ad830
ullahhab <61397521+ullahhab@users.noreply.github.com>
2020-04-26 22:17:08 -0500

    Update HBbasemodel.h

--- HBbasemodel.h
+++ HBbasemodel.h
@@ -11,4 +11,34 @@ namespace EMANE
   {
     namespace HeavyBall
     {
-      class BaseModel : public 
+      class BaseModel : public HeavyBallShimLayerImplementor,
+                        public SchedulerUser
+      {
+      public:
+        BaseModel(NEMId id,
+                  PlatformServiceProvider *platformSeviceProvider,
+                  RadioServiceProvider * pRadioServiceProvider,
+                  Scheduler * pScheduler,
+                  QueueManager * pQueueManager);
+                  
+       BaseModel();
+       
+       void initialize(Registrar & registrar) overriede;
+       
+       void configure(const configurationUpdate & update) override;
+       
+       void start() override;
+       
+       void postStart() override;
+       
+       void stop() override;
+       
+       void destroy() override;
+       
+       void processUpstreamControl(const ControlMessage & msgs) override;
+       
+       void processUpstreamPacket(const CommonMACHeader & header,
+                                  UpstreamPacket(const CommonMACHeader & pkt,
+                                  const ControlMessages & msgs) override;
+                                  
+       void 
