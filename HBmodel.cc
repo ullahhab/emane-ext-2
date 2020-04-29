502476606388b01be4f809b90b13c4b890e323a5
ullahhab <61397521+ullahhab@users.noreply.github.com>
2020-04-29 16:57:53 -0500

    Update HBmodel.cc

--- HBmodel.cc
+++ HBmodel.cc
@@ -1,2 +1,91 @@
 #include "emane/models/heavyball/HBmodel.h"
 #include "HBmodelimpl.h"
+
+
+EMANE::Models::HeavyBall::HBmodel::HBmodel(NEMId id,
+                                           PlatformServiceProvider * pPlatformServiceProvider,
+                                           RadioServiceProvider * pRadioServiceProvider,
+                                           Scheduler * pscheduler,
+                                           Queuemanager * pQueueManager):
+  
+  HeavyBallShimLayerImplementor{id, pPlatformServiceProvider, pRadioServiceProvider}
+  pImpl_{new Implementation{id,
+                            pPlatformServiceProvider,
+                            pRadioServiceProvider,
+                            pScheduler,
+                            pQueueManager,
+                            this}}
+{}
+
+
+EMANE::Models::HeavyBall::HBmodel::~HBmodel()
+{
+}
+
+void 
+EMANE::Model::HeavyBall::HBmodel::initialize(Registrar & registrar)
+{
+  pImpl_->initialize(registrar);
+}
+
+void
+EMANE::Models::HeavyBall::HBmodel::configure(const ConfigurationUpdate & update)
+{
+  pImple_->configure(update);
+}
+
+EMANE::Models::HeavyBall::HBmodel::start()
+{
+  pImpl_->start();
+}
+
+EMANE::Models::HeavyBall::HBmodel::postStart()
+{
+  pImpl_->postStart();
+}
+
+
+EMANE::Models::HeavyBall::HBmodel::stop()
+{
+  pImpl_->stop();
+}
+
+
+EMANE::Models::HeavyBall::HBmodel::destroy()
+{
+  pImpl_->destroy();
+}
+
+
+EMANE::Models::HeavyBall::HBmodel::processUpstreamControl(const ControlMessages & msgs)
+{
+  pImpl_->processUpstreamControl(msgs);
+}
+
+
+EMANE::Models::HeavyBall::HBmodel::processUpstreamPacket(const CommonMACHeader & hdr,
+                                                          UpstreamPacket & pkt,
+                                                          const ControlMessages & msgs)
+{
+  pImpl_->processUpstreamPacket(hdr,pkt,msgs);
+}
+
+EMANE::Models::HeavyBall::HBmodel::processDownStreamPacket(DownstreamPacket & pkt,
+                                                           const ControlMessages & msgs)
+{
+  pImpl_->processDownstreamPacket(msgs);
+}
+
+
+EMANE::Models::HeavyBall::HBmodel::processDownStreamConstrol(const ControlMessages & msgs)
+{
+  pImpl_->processDownstreamConstrol(msgs);
+}
+
+EMANE::Modles::HeavyBall::HBmodel::processEvent(const EventId & eventId,
+                                                const Serialization & serialization)
+{
+  pImpl_->processEvent(eventId,serializtion);
+}
+
+                                                      
