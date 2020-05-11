70cae3f66d629698ea3b9113e33c748444448103
ullahhab <61397521+ullahhab@users.noreply.github.com>
2020-05-11 08:50:13 -0500

    Update HBQueue.h

--- HBQueue.h
+++ HBQueue.h
@@ -1,6 +1,73 @@
-#include "emane/src/models/mac/tdma/queue.h"
+#ifndef EMANEMODELSHBQUEUE_HEADER_
+#define EMANEMODELSHBQUEUE_HEADER_
 
+#include "emane/types.h"
+#include "emane/downstreampacket.h"
+#include "emane/models/tdma/messagecomponent.h"
 
-std::map<std::uint64_t,size_t> getDestQueueLength();
+#include <cstdint>
+#include <map>
 
+namespace EMANE
+{
+  namespace Models
+  {
+    namespace HeavyBall
+    {
+      /**
+       * @class Queue
+       *
+       * @brief Downstream packet queue with both FIFO and
+       * By-Destination FIFO dequeue mechanisms.
+       */
+      class Queue
+      {
+      public:
+        Queue();
 
+        void initialize(std::uint16_t u16QueueDepth,
+                        bool bFragment,
+                        bool bAggregate,
+                        bool bIsControl);
+
+        std::pair<std::unique_ptr<DownstreamPacket>,bool>
+        enqueue(DownstreamPacket && pkt);
+
+        std::tuple<MessageComponents,
+                   size_t,
+                   std::list<std::unique_ptr<DownstreamPacket>>>
+          dequeue(size_t requestedBytes,NEMId destination,bool bDrop);
+
+        // packets, bytes
+        std::tuple<size_t,size_t> getStatus() const;
+
+        std::map<std::uint64_t,size_t> getDestQueueLength();
+
+      private:
+        class MetaInfo
+        {
+        public:
+          size_t index_{};
+          size_t offset_{};
+        };
+        using PacketQueue = std::map<std::uint64_t,
+                                     std::pair<DownstreamPacket *,MetaInfo *>>;
+        PacketQueue queue_;
+        std::map<NEMId,PacketQueue> destQueue_;
+        std::uint16_t u16QueueDepth_;
+        bool bFragment_;
+        std::uint64_t u64Counter_;
+        size_t currentBytes_;
+        bool bIsControl_;
+        bool bAggregate_;
+
+        std::pair<MessageComponent,size_t> fragmentPacket(DownstreamPacket * pPacket,
+                                                          MetaInfo * pMetaInfo,
+                                                          std::uint64_t u64Sequence,
+                                                          size_t bytes);
+      };
+    }
+  }
+}
+
+#endif // EMANEMODELSTDMAQUEUE_HEADER_
