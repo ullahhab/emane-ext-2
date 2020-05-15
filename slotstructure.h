ff2f428cabd6300d300a4515a16d730f886d81b3
ullahhab <61397521+ullahhab@users.noreply.github.com>
2020-05-15 17:07:23 -0500

    Create slotstructure.h

---
+++ slotstructure.h
@@ -0,0 +1,48 @@
+#ifndef EMANEEVENTSHBSLOTSTRUCTURE_HEADER_
+#define EMANEEVENTSHBSLOTSTRUCTURE_HEADER_
+
+#include "emane/types.h"
+#include <set>
+
+namespace EMANE
+{
+  namespace Events
+  {
+    class HBSlotStructure
+    {
+    public:
+      SlotStructure(std::uint64_t u64BandwidthHz,
+                    std::uint32_t u32FramePerMultiFrame,
+                    std::uint32_t u32SlotsPerFrame,
+                    const Microseconds & slotDuration,
+                    const Microseconds & slotOverhead
+                    const Beta);
+
+      SlotStructure();
+
+      std::uint64_t getBandwidth() const;
+
+      std::uint32_t getFramesPerMultiFrame() const;
+
+      std::uint32_t getSlotsPerFrame() const;
+
+      const Microseconds & getSlotDuration() const;
+
+      const Microseconds & getSlotOverhead() const;
+      
+      const Beta & getBetas() const;
+
+    private:
+      std::uint64_t u64BandwidthHz_;
+      std::uint32_t u32FramePerMultiFrame_;
+      std::uint32_t u32SlotsPerFrame_;
+      Microseconds slotDuration_;
+      Microseconds slotOverhead_;
+
+    };
+  }
+}
+
+#include "emane/events/slotstructure.inl"
+
+#endif
