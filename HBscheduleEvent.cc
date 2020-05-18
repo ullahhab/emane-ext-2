bc4c1363ca85ae83370ea103b12b8411889501d1
ullahhab <61397521+ullahhab@users.noreply.github.com>
2020-05-18 15:15:15 -0500

    Update HBscheduleEvent.cc

--- HBscheduleEvent.cc
+++ HBscheduleEvent.cc
@@ -6,191 +6,215 @@
 ////TODO::Misss matching close bracket somewhere check for it and also create getslotinfo and all other functions missing.
 class EMANE::Events::HBScheduleEvent::Implementation
 {
-  Implementation(const Serializaion & serialization):
+  public:
+  Implementation(const Serialization & serialization):
     bHasStructure_{}
-    {
-      EMANEMessage::HBScheduleEvent msg{};
-      if(!msg.ParseFromString(serialization))
+  {
+    EMANEMessage::HBScheduleEvent msg{};
+
+    if(!msg.ParseFromString(serialization))
       {
-        throw SerialationException("unable to deserialize : HBScheduleEvent");
+        throw SerializationException("unable to deserialize : HBScheduleEvent");
       }
-      
-     //TODO: Confirm what should a scheduler be doing exactly? Also that is something that would come along with some development
-      
-      if(msg.has_structure())
+
+    std::uint32_t u32FramesPerMultiFrame{};
+    std::uint32_t u32SlotsPerFrame{};
+
+    if(msg.has_structure())
       {
         const auto & structure = msg.structure();
-        
-        u32FramesPerMultiFrame = structure.framespermultifram();
-        u32SlotsPerFrame = strucure.slotsperframe();
-        
+
+        u32FramesPerMultiFrame = structure.framespermultiframe();
+        u32SlotsPerFrame = structure.slotsperframe();
+
         structure_ = SlotStructure{structure.bandwidthhz(),
                                    u32FramesPerMultiFrame,
                                    u32SlotsPerFrame,
                                    Microseconds{structure.slotdurationmicroseconds()},
-                                   Microseconds{structure.slotoverheadmicroseconds()}};
+                                   Microseconds{structure.slotoverheadmicroseconds()},
+                                   structure.beta()};
+
         bHasStructure_ = true;
-        slotInfos_.reserve(u32FreamesPerMultiFrame * u32SlotsPerFrame);
-                                   
+
+        slotInfos_.reserve(u32FramesPerMultiFrame * u32SlotsPerFrame);
       }
-      
-      if(bHasStructure_)
+
+    if(bHasStructure_)
       {
-        for(unsigned i = 0; i<u32FramesPerMultiFrame; ++i)
-        {
-          for(unsigned j=0; j<u32SlotPerFrame;++j)
+        // pre-built a complete schedule defaulting all slots to idle
+        for(unsigned i =  0; i < u32FramesPerMultiFrame; ++i)
           {
-            slotInfos_.push_back({SlotInfo::Type::IDLE,i,j});
+            for(unsigned j =  0; j < u32SlotsPerFrame; ++j)
+              {
+                slotInfos_.push_back({SlotInfo::Type::IDLE,i,j});
+              }
           }
-        }
       }
-      for(const auto & frame :msg.frames())
+
+    for(const auto & frame :msg.frames())
       {
-        std::uint32_t u32FrameIndex = frame.Idex();
-        if(bHasStructure_ && u32FrameIndex >= u32FramePerMultiFrame)
-        {
-          throw makeException<SerializationException>("HBScheduleEvent : Frame %lu index out of range",u32FrameIndex);
-        }
+        std::uint32_t u32FrameIndex = frame.index();
+
+        if(bHasStructure_ && u32FrameIndex >= u32FramesPerMultiFrame)
+          {
+            throw makeException<SerializationException>("HBScheduleEvent : Frame %lu index out of range",
+                                                        u32FrameIndex);
+          }
 
         std::set<std::uint32_t> presentSlots{};
 
-      
         for(const auto & slot : frame.slots())
-        {
-          SlotInfo::Type type{SlotInfos::Type::IDLE};
-          std::uint64_t u64FrequencyHz{};
-          std::uint64_t u64DataRatebps{};
-          std::uint8_t u8ServiceClass{};
-          double dPowerdBm{};
-          NEMId destination{};
-        
-          std::uint32_t u32SlotIndex >= slot.index();
-          if(bHasStructure_ && u32SlotIndex >= u32SlotsPerFrame)
           {
-            throw makeException<SerializationException>("HBScheduleEvent : Frame %lu Slot %lu slot index out of range",u32FrameIndex,u32SlotIndex);
-          }
-          
-          presentSlots.insert(u32SlotIndex);
-          switch(slot.type())
-          {
-            case EMANEMessage::HBScheduleEvent::Frame::Slot::Slot_tx;
+            SlotInfo::Type type{SlotInfo::Type::IDLE};
+            std::uint64_t u64FrequencyHz{};
+            std::uint64_t u64DataRatebps{};
+            std::uint8_t u8ServiceClass{};
+            double dPowerdBm{};
+            NEMId destination{};
+
+            std::uint32_t u32SlotIndex = slot.index();
+
+            if(bHasStructure_ && u32SlotIndex >= u32SlotsPerFrame)
               {
-                const auto & tx = slot.tx();
-                type = SlotInfos::Type::TX;
-                if(tx.has_frequencyhz())
-                {
-                  u64FrequencyHz = tx.frequencyhz();
-                }
-                else if(frame.has_frequencyhz())
-                {
-                  u64FrequencyHz = frame.frequencyhz();
-                }
-                else if(msg.has_frequencyhz())
-                {
-                  u64FrequencyHz = msg.frequencyhz();
-                }
-                else
-                {
-                  throw makeException<SerializtionException>("HBScheduleEvent : Frame %lu Slot %lu has undeterminable frequency", u32FrameIndex, u32SlotIndex);
-                }
-                
-                if(tx.has_dataratebps())
-                {
-                  u64DataRatebps = tx.dataratebps();
-                }
-                else if(frame.has_datararebps())
-                {
-                  u64DataRatebps = frame.dataratebps();
-                }
-                else if(msg.has_dataratebps())
-                {
-                  u64DataRatebps = msg.dataratebps();
-                }
-                else
-                {
-                  throw makeException<SerializationException>("HBScheduleEvent : Frame %lu Slot %lu has undeterminable datarate",u32FrameIndex,u32SlotIndex);
-                }
-                
-                if(tx.has_serviceclass())
-                {
-                  u8serviceClass = tx.serviceclass();
-                }
-                else if(frame.has_serviceclass())
-                {
-                  u8service  Class = frame.serviceclass();
-                }
-                else if(msg.has_serviceclass())
-                {
-                  u8ServiceClass = msg.serviceclass();
-                }
-                else
-                {
-                  throw makeException<SerializationException>("HBScheduleEvent : Frame %lu Slot %lu has undeterminable class",u32FrameIndex,u32SlotIndex);
-                }
-                
-                if(tx.has_powerdbm())
-                {
-                  dPowerdBm = tx.powerdbm();
-                }
-                else if(frame.has_powerdbm())
-                {
-                  dPowerdBm = frame.powerdbm();
-                }
-                else if(msg.has_powerdbm())
-                {
-                  dPowerdBm = msg.powerdbm();
-                }
-                else
-                {
-                  throw makeException<SerializationException>("HBScheduleEvent : Frame %lu Slot %lu has undeterminable power",
-                                                                    u32FrameIndex,
-                                                                    u32SlotIndex);
-                }
-                
-                        
-                if(tx.has_desitnaiton())
-                {
-                  destination = tx.destination())
-                }
+                 throw makeException<SerializationException>("HBScheduleEvent : Frame %lu Slot %lu slot index out of range",
+                                                             u32FrameIndex,
+                                                             u32SlotIndex);
               }
-              break;
-             case EMANEMessage::HBScheduleEvent::Frame::Slot::SLOT_RX:
+
+            presentSlots.insert(u32SlotIndex);
+
+            switch(slot.type())
+              {
+              case EMANEMessage::HBScheduleEvent::Frame::Slot::SLOT_TX:
+                {
+                  const auto & tx = slot.tx();
+
+                  type = SlotInfo::Type::TX;
+
+                  if(tx.has_frequencyhz())
+                    {
+                      u64FrequencyHz = tx.frequencyhz();
+                    }
+                  else if(frame.has_frequencyhz())
+                    {
+                      u64FrequencyHz = frame.frequencyhz();
+                    }
+                  else if(msg.has_frequencyhz())
+                    {
+                      u64FrequencyHz = msg.frequencyhz();
+                    }
+                  else
+                    {
+                      throw makeException<SerializationException>("HBScheduleEvent : Frame %lu Slot %lu has undeterminable frequency",
+                                                                  u32FrameIndex,
+                                                                  u32SlotIndex);
+                    }
+
+                  if(tx.has_dataratebps())
+                    {
+                      u64DataRatebps = tx.dataratebps();
+                    }
+                  else if(frame.has_dataratebps())
+                    {
+                      u64DataRatebps = frame.dataratebps();
+                    }
+                  else if(msg.has_dataratebps())
+                    {
+                      u64DataRatebps = msg.dataratebps();
+                    }
+                  else
+                    {
+                      throw makeException<SerializationException>("HBScheduleEvent : Frame %lu Slot %lu has undeterminable datarate",
+                                                                  u32FrameIndex,
+                                                                  u32SlotIndex);
+                    }
+
+
+                  if(tx.has_serviceclass())
+                    {
+                      u8ServiceClass = tx.serviceclass();
+                    }
+                  else if(frame.has_serviceclass())
+                    {
+                      u8ServiceClass = frame.serviceclass();
+                    }
+                  else if(msg.has_serviceclass())
+                    {
+                      u8ServiceClass = msg.serviceclass();
+                    }
+                  else
+                    {
+                      throw makeException<SerializationException>("HBScheduleEvent : Frame %lu Slot %lu has undeterminable class",
+                                                                  u32FrameIndex,
+                                                                  u32SlotIndex);
+                    }
+
+
+                  if(tx.has_powerdbm())
+                    {
+                      dPowerdBm = tx.powerdbm();
+                    }
+                  else if(frame.has_powerdbm())
+                    {
+                      dPowerdBm = frame.powerdbm();
+                    }
+                  else if(msg.has_powerdbm())
+                    {
+                      dPowerdBm = msg.powerdbm();
+                    }
+                  else
+                    {
+                      throw makeException<SerializationException>("HBScheduleEvent : Frame %lu Slot %lu has undeterminable power",
+                                                                  u32FrameIndex,
+                                                                  u32SlotIndex);
+                    }
+
+
+                  if(tx.has_destination())
+                    {
+                      destination = tx.destination();
+                    }
+                }
+                break;
+
+              case EMANEMessage::HBScheduleEvent::Frame::Slot::SLOT_RX:
                 {
                   const auto & rx = slot.rx();
+
                   type = SlotInfo::Type::RX;
-                  
+
                   if(rx.has_frequencyhz())
-                  {
-                    u64FrequencyHz = rx.frequecnyhz();
-                  }
+                    {
+                      u64FrequencyHz = rx.frequencyhz();
+                    }
                   else if(frame.has_frequencyhz())
-                  {
-                    u64FrequencyHz = frame.frequencyhz();
-                  }
-                  else if(msg.has_frequncyhz())
-                  {
-                    u64FrequencyHz = msg.frequencyhz();
-                  }
+                    {
+                      u64FrequencyHz = frame.frequencyhz();
+                    }
+                  else if(msg.has_frequencyhz())
+                    {
+                      u64FrequencyHz = msg.frequencyhz();
+                    }
                   else
-                  {
-                    throw makeException<SerializationException>("TDMAScheduleEvent : Frame %lu Slot %lu has undeterminable frequency",
-                                                                    u32FrameIndex,
-                                                                    u32SlotIndex);
-                  }
+                    {
+                      throw makeException<SerializationException>("HBScheduleEvent : Frame %lu Slot %lu has undeterminable frequency",
+                                                                  u32FrameIndex,
+                                                                  u32SlotIndex);
+                    }
                 }
                 break;
-                  
-                case EMANEMessage::HBScheduleEvent::Frame::Slot::SLOT_IDLE:
+
+              case EMANEMessage::HBScheduleEvent::Frame::Slot::SLOT_IDLE:
                 type = SlotInfo::Type::IDLE;
                 break;
-                }
-                
-                
-                frequencies_.insert(u64FrequncyHz);
-                
-                if(bHasStructure_)
-                {
-                  slotInfos_[u32FrameIndex * u32SlotPerFrame + u32slotIndex] = 
+              }
+
+
+            frequencies_.insert(u64FrequencyHz);
+
+            if(bHasStructure_)
+              {
+                slotInfos_[u32FrameIndex * u32SlotsPerFrame + u32SlotIndex] =
                   {type,
                    u32FrameIndex,
                    u32SlotIndex,
@@ -199,52 +223,55 @@ class EMANE::Events::HBScheduleEvent::Implementation
                    u8ServiceClass,
                    dPowerdBm,
                    destination};
-                }
-                else
-                {
-                  slotInfos_.push_back({type,
-                                        u32FrameIndex,
-                                        u32SlotIndex,
-                                        u64FrequencyHz,
-                                        u64DataRatebps,
-                                        u8ServiceClass,
-                                        dPowerdBm,
-                                        destination});
-                }
               }
-              if(bHasStructure_)
+            else
               {
-                for(unsigned i = 0; i<u32SlotsPerFrame; ++i)
-                {
-                  if(!presentSlots.count(i))
+                slotInfos_.push_back({type,
+                      u32FrameIndex,
+                      u32SlotIndex,
+                      u64FrequencyHz,
+                      u64DataRatebps,
+                      u8ServiceClass,
+                      dPowerdBm,
+                      destination});
+              }
+          }
+
+        if(bHasStructure_)
+          {
+            // add RX slot info for any missing slot
+            for(unsigned i = 0; i < u32SlotsPerFrame; ++i)
+              {
+                if(!presentSlots.count(i))
                   {
                     std::uint64_t u64FrequencyHz{};
-                    if(frame.has_frequencyHz = frame.frequencyhz())
-                    {
-                      u64FrequencyHz = frame.frequencyhz();
-                    }
+
+                    if(frame.has_frequencyhz())
+                      {
+                        u64FrequencyHz = frame.frequencyhz();
+                      }
                     else if(msg.has_frequencyhz())
-                    {
-                      u64FrequencyHz = msg.frequencyhz();
-                    }
+                      {
+                        u64FrequencyHz = msg.frequencyhz();
+                      }
                     else
-                    {
-                      throw makeException<SerializationException>("HBScheduleEvent : Frame %lu Slot %lu has undeterminable frequency",
-                                                                       u32FrameIndex,
-                                                                       i);
-                    }
-                    
+                      {
+                        throw makeException<SerializationException>("HBScheduleEvent : Frame %lu Slot %lu has undeterminable frequency",
+                                                                    u32FrameIndex,
+                                                                    i);
+                      }
+
                     frequencies_.insert(u64FrequencyHz);
-                    slotInfos_[u32FrameIndex * u32SlotsPerFrame +i] = {SlotInfos::Type::RX,
-                                                                       u32FrameIndex,
-                                                                       i,
-                                                                       u64FrequencyHz};
+
+                    slotInfos_[u32FrameIndex * u32SlotsPerFrame + i] = {SlotInfo::Type::RX,
+                                                                        u32FrameIndex,
+                                                                        i,
+                                                                        u64FrequencyHz};
                   }
-                }
               }
+          }
       }
-    }
-}
+  }
 
 
    
