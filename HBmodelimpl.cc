9eb863e52be80d8898aed45f0aa941812f961107
ullahhab <61397521+ullahhab@users.noreply.github.com>
2020-05-19 08:32:40 -0500

    Update HBmodelimpl.cc

--- HBmodelimpl.cc
+++ HBmodelimpl.cc
@@ -1326,16 +1326,12 @@ EMANE::NEMId EMANE::Models::HeavyBall::HBmodel::Implementation::getDstByMaxWeigh
   //TODO:: Yet to be implemented
   
   //TODO:: declare new files to use function getDstQueueLength();
+{
+
   auto qls = pQueueManager_->getDestQueueLength(0);
   for (auto it=qls.begin(); it!=qls.end(); ++it) 
   {
-    LOGGER_STANDARD_LOGGING(pPlatformService_->logService(),
-                            DEBUG_LEVEL,
-                            "MACI %03hu HeavyBall::HBModel::%s Queue %hu has size %zu",
-                            id_,
-                            __func__,
-                            it->first,
-                            it->second);
+    if (65535 == it->first && it->second > 2) return 65535; 
   }
 
   EMANE::NEMId nemId{0};
@@ -1343,38 +1339,132 @@ EMANE::NEMId EMANE::Models::HeavyBall::HBmodel::Implementation::getDstByMaxWeigh
 
   if (EMANE::Utils::initialized) 
   {
+    counter_++;
+    std::string msg = ""; 
     EMANE::Events::Pathlosses pe = EMANE::Utils::pathlossesHolder;
     for (auto const& it: pe)
     {
-      auto ql = qls.find(it.getNEMId());
+      if (msg != ""){
+        msg.append(",");
+      }
+      auto id = it.getNEMId();
+      auto ql = qls.find(id);
+      msg.append(std::to_string(id));
       if (ql == qls.end())
       {
+        msg.append(":0");
         continue;
       }
-      double score = EMANE::Utils::DB_TO_MILLIWATT(0-it.getForwardPathlossdB()) * ql->second;
+
+      double weight = lastWeight_[id] + ql->second - lastQueueLength_[id] + BETA_ * (lastWeight_[id] - lastLastWeight_[id]+ ql->second + lastLastQueueLength_[id] - 2 * lastQueueLength_[id]);
+
+      if (weight < 0)
+        {
+          weight = 0;
+        }
+      
+      weightT_[id] += lastWeight_[id];
+
+      lastLastWeight_[id] = lastWeight_[id];
+      lastWeight_[id] = weight;
+      lastLastQueueLength_[id] = lastQueueLength_[id];
+      lastQueueLength_[id] = ql->second;
+
+
+      msg.append(":");
+      msg.append(std::to_string(weightT_[id] / counter_));
+
+      // todo: change 110 to txpower - noise
+      double snr = EMANE::Utils::DB_TO_MILLIWATT(110-it.getForwardPathlossdB());
+      // double score = log2(1.0 + snr) * ql->second;
+      double score = log2(1.0 + snr) * weight;
       if (score > maxScore)
       {
-        nemId = it.getNEMId();
+        nemId = id;
         maxScore = score;
       }
       LOGGER_STANDARD_LOGGING(pPlatformService_->logService(),
                               DEBUG_LEVEL,
-                              "MACI %03hu HeavyBall::HBModel::%s pathloss of %hu is %f, and score: %f!",
+                              "MACI %03hu TDMA::BaseModel::%s dest: %hu, queuelength: %zu, weight: %f, snr: %f, and score: %f!",
                               id_,
                               __func__,
                               it.getNEMId(),
-                              it.getForwardPathlossdB(),
+                              ql->second,
+                              weight,
+                              snr,
                               score);
-    }         
+    }
+
+    if (counter_ == 1)
+     {
+        // create socket and ready for send data out.
+        counter_ = 0;
+        for (int i = 0; i < 10; i++)
+          {
+            weightT_[i] = 0;
+          }
+        int sock_fd = -1;
+        char buf[MAXDATASIZE];
+        int recvbytes, sendbytes, len;
+
+        in_addr_t server_ip = inet_addr("127.0.0.1");
+        in_port_t server_port = 10036;
+
+
+        if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+          LOGGER_STANDARD_LOGGING(pPlatformService_->logService(),
+                                  DEBUG_LEVEL,
+                                  "MACI %03hu TDMA::BaseModel::%s Socket creation error!",
+                                  id_,
+                                  __func__);
+        }
+
+        long flag = 1;
+        setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, (char *)&flag, sizeof(flag));
+
+        struct sockaddr_in server_addr;
+        server_addr.sin_addr.s_addr = server_ip;
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(server_port);
+        // printf("Try to connect server(%s:%u)\n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
+
+        if(connect(sock_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1) {
+          LOGGER_STANDARD_LOGGING(pPlatformService_->logService(),
+                                  DEBUG_LEVEL,
+                                  "MACI %03hu TDMA::BaseModel::%s Connection Failed!",
+                                  id_,
+                                  __func__);
+          close(sock_fd);  
+          return nemId;
+        }  
+
+        // printf("Connect server success(%s:%u)\n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
+        if(send(sock_fd, msg.c_str(), msg.size(), 0) == -1) {
+            LOGGER_STANDARD_LOGGING(pPlatformService_->logService(),
+                        DEBUG_LEVEL,
+                        "MACI %03hu TDMA::BaseModel::%s Send Failed!",
+                        id_,
+                        __func__);
+        }
+        if((recvbytes=recv(sock_fd, buf, MAXDATASIZE, 0)) == -1) {  
+          LOGGER_STANDARD_LOGGING(pPlatformService_->logService(),
+                                  DEBUG_LEVEL,
+                                  "MACI %03hu TDMA::BaseModel::%s Connection recv Failed!",
+                                  id_,
+                                  __func__);
+        }
+        buf[recvbytes] = '\0';
+        LOGGER_STANDARD_LOGGING(pPlatformService_->logService(),
+                                  DEBUG_LEVEL,
+                                  "MACI %03hu TDMA::BaseModel::%s \"%s\" recived!",
+                                  id_,
+                                  __func__,
+                                  buf);
+        close(sock_fd);  
+     }
+     
   }
-  else
-  {
-    LOGGER_STANDARD_LOGGING(pPlatformService_->logService(),
-                            DEBUG_LEVEL,
-                            "MACI %03hu HeavyBall::HBModel::%s pathloss not initialized yet!",
-                            id_,
-                            __func__);
-  }
+
   return nemId;
 
 }
